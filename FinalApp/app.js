c12530d493f8923549b27527a1ad78d588a06322
Andreas Kastis <andreas.ksts@gmail.com>
2019-07-30 23:18:39 +0300

    Test routing

--- FinalApp/app.js
+++ FinalApp/app.js
@@ -5,6 +5,17 @@ app.get("/",function(req, res){
 	res.send("Hello World");
 });
 
+app.get("/joke",function(req,res){
+	var joke="A nice joke example!";
+	res.send(joke);
+});
+
+
+app.get("/random_num", function(req, res){
+ var num = Math.floor((Math.random() * 10) + 1);
+ res.send("Your lucky number is " + num);
+});
+
 app.listen(8080, function(){
 	console.log("Server running on 8080!");
 });
