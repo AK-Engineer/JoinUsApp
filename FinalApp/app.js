a3197bd2db2a4c056623c7bdb4a3a57292a939fe
Andreas Kastis <andreas.ksts@gmail.com>
2019-07-30 23:37:39 +0300

    Connecting Express and MySQl

--- FinalApp/app.js
+++ FinalApp/app.js
@@ -1,8 +1,22 @@
 var express = require('express');
+var mysql = require('mysql');
 var app = express();
 
-app.get("/",function(req, res){
-	res.send("Hello World");
+// Creating MySql Connection
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',  //your username
+  database : 'join_us'         //the name of your db
+});
+
+
+app.get("/", function(req, res){
+ var q = 'SELECT COUNT(*) as count FROM users';
+ connection.query(q, function (error, results) {
+ if (error) throw error;
+ var msg = "We have " + results[0].count + " users";
+ res.send(msg);
+ });
 });
 
 app.get("/joke",function(req,res){
